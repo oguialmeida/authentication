ba47631a17fa3699a6fd6ff70639f83ecbfc9a41
Guilherme Gomes <guilherme.almeida6875@gmail.com>
2022-09-29 11:16:33 -0300

    Excluindo arquivo desnecessario

--- main.py
+++
@@ -1,7 +0,0 @@
-# Tem o intuito de iniciar o servidor e a aplicação simultaneamente
-from server import servidor as sv
-from interface import janela as jn
-
-if __name__ == "__main__":
-    sv.servidorPrincipal()
-    jn.iniciarTela()
